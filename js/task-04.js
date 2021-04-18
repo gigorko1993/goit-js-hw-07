db14e37f275339dd92fbacc5ec6ca437e095fe02
gigorko1993 <gigorko1993@gmail.com>
2021-04-18 18:37:08 +0300

    Finished task 04

---
+++ js/task-04.js
@@ -0,0 +1,27 @@
+const counterArray = {
+    value: 0,
+
+    onDecrementButtonEvent(){
+        this.value += 1;
+    },
+    
+    onIncrementButtonEvent(){
+        this.value -= 1;
+    },
+};
+
+const counterButtonsEl = document.querySelectorAll('#counter button');
+const counterValue = document.querySelector('#value');
+
+const decrementCounterBtn = counterButtonsEl[0];
+const incrementCounterBtn = counterButtonsEl[1];
+
+decrementCounterBtn.addEventListener('click', () => {
+    counterArray.onDecrementButtonEvent();
+    counterValue.textContent = counterArray.value;
+});
+incrementCounterBtn.addEventListener('click', () => {
+    counterArray.onIncrementButtonEvent();
+    counterValue.textContent = counterArray.value;
+});
+
