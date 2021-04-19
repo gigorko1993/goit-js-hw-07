0c2c6d4e403e09f79f2a8f7db960b76e0014ee81
gigorko1993 <gigorko1993@gmail.com>
2021-04-19 16:20:08 +0300

    Finished task 06

---
+++ js/task-06.js
@@ -0,0 +1,15 @@
+const refs = {
+    input: document.querySelector('#validation-input'),
+}
+
+refs.input.addEventListener('change', onInputChange);
+
+function onInputChange(event) {
+    event.currentTarget.value.length >= refs.input.dataset.length
+        ? refs.input.classList.add('valid')
+        : refs.input.classList.remove('valid');
+    
+    event.currentTarget.value.length < refs.input.dataset.length
+        ? refs.input.classList.add('invalid')
+        : refs.input.classList.remove('invalid');
+}
