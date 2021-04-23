8d03ed219822868e6c9c5b9f31c4fa6cf01f4fac
gigorko1993 <gigorko1993@gmail.com>
2021-04-23 19:45:52 +0300

    make refactoring 05 task

--- js/task-05.js
+++ js/task-05.js
@@ -3,8 +3,6 @@ const refs = {
     output: document.querySelector('#name-output'),
 };
 
-refs.input.addEventListener('input', onInputChange);
-
-function onInputChange(event) {
-    refs.output.textContent = event.currentTarget.value;
-}
+refs.input.addEventListener('input', (event) => { !event.currentTarget.value.replace(/\s/g, '').length
+    ? refs.output.textContent = 'незнакомец'
+    : refs.output.textContent = event.currentTarget.value; });
