ccfe0dd523769649ebd3867d0c99db98495a4ac5
gigorko1993 <gigorko1993@gmail.com>
2021-04-23 11:52:25 +0300

    make refactoring of 1 task

--- js/task-01.js
+++ js/task-01.js
@@ -1,15 +1,17 @@
 const categoryEl = document.querySelectorAll('li.item');
+
 function declOfCategory(number, words) {  
     return words[(number % 100 > 4 && number % 100 < 20) ? 2 : [2, 0, 1, 1, 1, 2][(number % 10 < 5) ? number % 10 : 5]];
 }
-let numberOfCategory = categoryEl.length;
-console.log(`В списке ${numberOfCategory} ${declOfCategory(numberOfCategory, ['категория', 'категории', 'категорий'])}.`);
 
-for (let i = 0; i < numberOfCategory; i += 1) {
-    const categoryNameEl = categoryEl[i].firstElementChild.textContent;
-const categoryNumberOfElementsEl = categoryEl[i].lastElementChild.children.length;
+console.log(`В списке ${categoryEl.length} ${declOfCategory(categoryEl.length, ['категория', 'категории', 'категорий'])}.`);
 
-    console.log(`\u2022 Категория: ${categoryNameEl}
-\u2022 Количество элементов: ${categoryNumberOfElementsEl}`)
-}
+function countCategory(lists) {
+    return lists.forEach(element => {
+        console.log(`\u2022 Категория: ${element.firstElementChild.textContent}
+\u2022 Количество элементов: ${element.lastElementChild.children.length}`)
+    });
+};
+
+countCategory(categoryEl);
 
